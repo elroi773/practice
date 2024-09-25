766eda63debba3841e5a56f651f0d456e5dbcf46
김이레 <165885725+elroi773@users.noreply.github.com>
2024-09-26 07:06:07 +0900

    모바일 청첩장 연습중 js 파일

---
+++ 모바일 청첩장.js
@@ -0,0 +1,7 @@
+document.querySelector('.accept-btn').addEventListener('click', () => {
+    alert("Thank you for attending!");
+});
+
+document.querySelector('.decline-btn').addEventListener('click', () => {
+    alert("Sorry you can't make it!");
+});
