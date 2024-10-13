a3c292c7f6cf4a094f8a3ffa1bd3c27654666eec
김이레 <165885725+elroi773@users.noreply.github.com>
2024-10-14 08:24:29 +0900

    대신 전해 드립니다 DB 연결 자바 파일

---
+++ User.java
@@ -0,0 +1,51 @@
+package user;
+
+public class User {
+	private String userID;
+	private String userPassword;
+	private String userName;
+	private String userGender;
+	private String userEmaill;
+	private String userschool;
+
+	
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmaill() {
+		return userEmaill;
+	}
+	public void setUserEmaill(String userEmaill) {
+		this.userEmaill = userEmaill;
+	}
+	public String getUserschool() {
+		return userschool;
+	}
+	public void setUserschool(String userschool) {
+		this.userschool = userschool;
+	}
+	
+	
+}
