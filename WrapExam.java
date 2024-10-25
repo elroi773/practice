a994321eb631d0d4ea8d1703601ef964af0bbd98
김이레 <165885725+elroi773@users.noreply.github.com>
2024-10-25 10:05:25 +0900

    주리쌤과 함께하는 Wrap 패키지 복습용

---
+++ WrapExam.java
@@ -0,0 +1,19 @@
+package javaclass;
+
+public class WrapExam {
+
+	public static void main(String[] args) {
+		Integer wrapi = new Integer(100);
+		Double wrapd = new Double(55.7);
+		
+		int i =100;
+		double d = 55.7;
+		double dl;
+		dl = d+i;
+		System.out.println(i+d);
+		System.out.println(Integer.toString(i)+Double.toString(dl));
+		System.out.println(wrapi.toString()+wrapd.toString());
+
+	}
+
+}
