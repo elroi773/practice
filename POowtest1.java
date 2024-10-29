003afdb8491d144d6c22c0e3dc944635beb7fbd3
김이레 <165885725+elroi773@users.noreply.github.com>
2024-10-29 20:01:18 +0900

    주리쌤과 함께하는 피타고라스를 사용해서 길이 구하기 실습 코드

---
+++ POowtest1.java
@@ -0,0 +1,18 @@
+package javaclass;
+
+public class POowtest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x1 = 0;
+		int y1 = 0;
+		int x2 = 3;
+		int y2 = 4;
+		
+		
+		
+		
+		System.out.println(Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2)));
+	}
+
+}
