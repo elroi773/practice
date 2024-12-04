af9350c0a6308ba9425d255c38d961640574fb27
김이레 <165885725+elroi773@users.noreply.github.com>
2024-12-04 23:00:55 +0900

    주리쌤과 함께한 자바 시간 실습 코드

---
+++ ReverseExam.java
@@ -0,0 +1,13 @@
+package javaclass;
+
+public class ReverseExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sbbb1 = new StringBuffer("java Programaing");
+		StringBuffer sbbb2 = null;
+		sbbb2 = sbbb1.reverse();
+		System.out.println("sb2 = "+ sbbb2);
+	}
+
+}
