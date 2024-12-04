77d67330bb06d145a79c2fe4a0102f61e14b0433
김이레 <165885725+elroi773@users.noreply.github.com>
2024-12-04 23:02:09 +0900

    주리쌤과 함께한 자바 시간 실습 코드

---
+++ ToStringExam.java
@@ -0,0 +1,14 @@
+package javaclass;
+
+public class ToStringExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sbbb1 = new StringBuffer("java programming");
+		String str = null;
+		str = sbbb1.toString();
+		System.out.println("str.toUpperCase() = " + str.toUpperCase());
+		System.out.println("str.toLowerCase() = " + str.toLowerCase());
+	}
+
+}
