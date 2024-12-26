47eb198365b30d76eaccd05cfaf01723612508db
김이레 <165885725+elroi773@users.noreply.github.com>
2024-12-26 21:50:43 +0900

    주리쌤과 함께 한 자바 실습 코드

---
+++ SquareExam.java
@@ -0,0 +1,17 @@
+package javaclass;
+
+public class SquareExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.sqrt(9));
+		System.out.println(Math.sqrt(9.0f));
+
+		System.out.println(Math.sqrt(9.0));
+
+		System.out.println(Math.sqrt(-9.0));
+
+		
+	}
+
+}
