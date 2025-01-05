e38bb0d2c89c390ed19cef4dd6521f152adc3ce2
김이레 <165885725+elroi773@users.noreply.github.com>
2025-01-05 23:17:54 +0900

    친구 소개 페이지

---
+++ mobile1.js
@@ -0,0 +1,20 @@
+
+const playlist = document.getElementById('playlist');
+        const audioPlayer = document.getElementById('audioPlayer');
+        const playerContainer = document.getElementById('player-container');
+
+        playlist.addEventListener('click', function(e) {
+            if (e.target && e.target.nodeName === 'LI') {
+                const songSrc = e.target.getAttribute('data-src');
+                audioPlayer.src = songSrc;
+                audioPlayer.play();
+
+                // Add animation class
+                playerContainer.classList.add('active');
+            }
+        });
+
+        audioPlayer.addEventListener('pause', function() {
+            // Remove animation class when paused
+            playerContainer.classList.remove('active');
+        });
