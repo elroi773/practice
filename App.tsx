a66d7f20ecc877df158149ecc093726dd06e48ec
김이레 <165885725+elroi773@users.noreply.github.com>
2025-01-19 22:16:55 +0900

    리엑트 첫 파일

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FirstReact from "./components/firstreact";
+
+function App() {
+  return (
+    <div>
+      {/* FirstReact 컴포넌트를 렌더링 */}
+      <main>
+        <FirstReact />
+      </main>
+    </div>
+  );
+}
+
+export default App;
